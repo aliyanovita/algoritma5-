e910f00804b942217b8b5c4e7b49dfb1e12e2a90
aliyanovita <aliyaksp3@gmail.com>
2024-10-08 10:16:36 +0700

    01 fungsi perkenalan.php

---
+++ 01 fungsi perkenalan.php
@@ -0,0 +1,16 @@
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikum, ";
+  echo "Perkenalkan, nama kita Ahmadi<br/>";
+  echo "Senang berkenalan dengan anda<br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+echo "<hr>";
+// memanggilnya lagi
+perkenalan();
+?>
+
+echo "<p><strong>by:aliya novita"
